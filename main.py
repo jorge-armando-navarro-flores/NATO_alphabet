afc3ea5a1475ae0bebf41717f215c43b926591d8
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-10-08 12:50:22 -0500

    try

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import pandas
 
 
 nato_alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet_data.iterrows()}
 
-word_input = input("Enter a word: ")
-phonetic_name = [nato_alphabet_dict[letter.upper()] for letter in word_input]
-print(phonetic_name)
+userInput = True
+while userInput:
+    word_input = input("Enter a word: ").upper()
+    try:
+        phonetic_name = [nato_alphabet_dict[letter] for letter in word_input]
+        userInput = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        print(phonetic_name)
